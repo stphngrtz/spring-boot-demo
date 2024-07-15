bfe8adc356a0e18fcf0401e61a057ab6e6aeed7b
Stephan Goertz <stephan.goertz@gmail.com>
2024-07-15 14:34:12 +0200

    collecting requests

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -12,13 +20,24 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-
 	@RestController
-	public static class HelloController {
+	public static class DemoController {
 
-		@RequestMapping("/")
-		String hello() {
-			return "Hello World";
+		AtomicInteger counter = new AtomicInteger(0);
+		String[] messages = new String[] {"/", "/", "/"};
+
+		@RequestMapping(path = "/", method = RequestMethod.GET)
+		Map<String, Object> get() {
+			return Map.of("counter", counter.get(), "messages", messages);
+		}
+
+		@RequestMapping(path = "/", method = RequestMethod.POST)
+		void post(@RequestBody String data) {
+			counter.incrementAndGet();
+
+			messages[2] = messages[1];
+			messages[1] = messages[0];
+			messages[0] = data;
 		}
 	}
 }
